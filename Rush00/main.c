e16d6c34d146a65aed2a7157bef5b8430a07f3ee
Timothy Tas <162969953+tazerotu@users.noreply.github.com>
2024-03-17 10:36:29 +0100

    Update main.c

--- Rush00/main.c
+++ Rush00/main.c
@@ -1,47 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttas <ttas@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/16 10:47:51 by ttas              #+#    #+#             */
+/*   Updated: 2024/03/17 10:26:47 by ttas             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <unistd.h>
+void	rush(int y, int x);
 
-void ft_putchar(char c)
+int	main(void)
 {
-    write(1, &c, 1);
-}
-
-void rush(int row, int col)
-{
-    int icol = 1;
-    int irow = 1;
-    char c;
-    while (irow <= row)
-        while(icol <= col)
-    {
-        {
-            if ((icol == 1 && irow == 1) || (icol == col && irow == 1) || (icol == 1 && irow == row) || (icol == col && irow = row))
-            {
-                ft_putchar('o');
-            }
-            if ((icol != 1 && icol != col) && (irow == 1 || irow == row))
-            {
-                ft_putchar('-');
-            }
-            if ((icol == 1 || icol == col) && (irow != 1 || irow != row))
-            {
-                ft ft_putchar('|');
-            }
-            if (icol == col)
-            {
-                icol = 0;
-                write(1, '\n', 2);
-            }
-            icol++;
-        }
-        irow++;
-    }
-    
-}
-
-
-int main(void)
-{
-    rush(5,5);
-    return(0);
+	rush(5, 1);
+	return (0);
 }
