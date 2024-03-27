fef72be125edb82d0b4c99b99c393b67e743dc36
Timothy Tas <162969953+tazerotu@users.noreply.github.com>
2024-03-27 11:02:17 +0100

    Create ft_print_comb2.c

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttas <ttas@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/13 13:35:20 by ttas              #+#    #+#             */
+/*   Updated: 2024/03/19 10:11:35 by ttas             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	display(int nb)
+{
+	int	first;
+	int	second;
+
+	if (nb > 9)
+	{
+		first = nb / 10;
+		second = nb % 10;
+		ft_putchar(first + 48);
+		ft_putchar(second + 48);
+	}
+	else
+	{
+		ft_putchar('0');
+		ft_putchar(nb + 48);
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	o;
+
+	i = 0;
+	while (i <= 99)
+	{
+		o = i + 1;
+		while (o <= 99)
+		{
+			display(i);
+			ft_putchar(' ');
+			display(o);
+			if (i < 98 || o < 99)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			++o;
+		}
+		++i;
+	}
+}
