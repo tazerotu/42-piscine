5147f22583b21f7af9a816f6ab0650b446779233
Timothy Tas <162969953+tazerotu@users.noreply.github.com>
2024-03-27 11:15:47 +0100

    Create ft_str_is_upppercase.c

---
+++ C02/ex05/ft_str_is_upppercase.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttas <ttas@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/19 09:12:11 by ttas              #+#    #+#             */
+/*   Updated: 2024/03/26 10:06:33 by ttas             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+// #include <string.h>
+
+int	ft_str_is_uppercase(char *str)
+{
+	int	count;
+	int	uppercase;
+
+	uppercase = 1;
+	count = 0;
+	if (str[count] == '\0')
+		return (uppercase);
+	while (str[count] != '\0')
+	{
+		if ((str[count] < 'A' || str[count] > 'Z'))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (uppercase);
+}
