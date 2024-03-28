b414362e497e070ab6231bfbc1e8c3d084d6518c
Timothy Tas <162969953+tazerotu@users.noreply.github.com>
2024-03-28 10:33:54 +0100

    Create ft_is_prime.c

---
+++ C05/ex06/ft_is_prime.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_prime.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttas <ttas@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/21 12:58:35 by ttas              #+#    #+#             */
+/*   Updated: 2024/03/21 12:59:05 by ttas             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+
+int	ft_is_prime(int nb)
+{
+	int	mod;
+
+	mod = 2;
+	if (nb <= 1)
+		return (0);
+	while (mod < nb)
+	{
+		if (nb % mod == 0)
+		{
+			return (0);
+		}
+		mod++;
+	}
+	return (1);
+}
+
+/*
+int	main(int argc, char *argv[])
+{
+	int	result;
+
+	if (argc != 2)
+	{
+		printf("Erreur, vous ne pouvez pas fournir plus qu'un parametre");
+	}
+	else
+	{
+		printf("le parametre donne est : %s\n\n", argv[1]);
+		result = ft_is_prime(atoi(argv[1]));
+		printf("le resultat apres la fonction est : %d\n", result);
+	}
+	return (0);
+}
+*/
